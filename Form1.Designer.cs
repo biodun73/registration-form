dcdc3200ac8bcefaece8bd44d2da6cdb70ca4d67
Abiodun  Ajayi <biodun73@users.noreply.github.com>
2023-11-23 19:47:17 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace insertand_fetchdata
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtFirstname = new System.Windows.Forms.TextBox();
+            this.txtLastname = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cboBox = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtWorker = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkViolet;
+            this.label1.Location = new System.Drawing.Point(273, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(459, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Workers Registration Form ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(108, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 33);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Firstname";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(108, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 33);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Lastname";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(108, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 33);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Status";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(108, 229);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 33);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Address";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(112, 285);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 33);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Email";
+            // 
+            // txtFirstname
+            // 
+            this.txtFirstname.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFirstname.Location = new System.Drawing.Point(349, 63);
+            this.txtFirstname.Name = "txtFirstname";
+            this.txtFirstname.Size = new System.Drawing.Size(268, 39);
+            this.txtFirstname.TabIndex = 6;
+            // 
+            // txtLastname
+            // 
+            this.txtLastname.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLastname.Location = new System.Drawing.Point(349, 122);
+            this.txtLastname.Name = "txtLastname";
+            this.txtLastname.Size = new System.Drawing.Size(268, 39);
+            this.txtLastname.TabIndex = 7;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAddress.Location = new System.Drawing.Point(349, 223);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(268, 39);
+            this.txtAddress.TabIndex = 9;
+            // 
+            // txtMail
+            // 
+            this.txtMail.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMail.Location = new System.Drawing.Point(349, 279);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(268, 39);
+            this.txtMail.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Turquoise;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(118, 341);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(225, 43);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Add Data";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 464);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(864, 254);
+            this.tableLayoutPanel1.TabIndex = 12;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(858, 248);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // cboBox
+            // 
+            this.cboBox.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboBox.FormattingEnabled = true;
+            this.cboBox.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.cboBox.Location = new System.Drawing.Point(349, 175);
+            this.cboBox.Name = "cboBox";
+            this.cboBox.Size = new System.Drawing.Size(268, 39);
+            this.cboBox.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(108, 15);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(125, 33);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "WorkerID";
+            // 
+            // txtWorker
+            // 
+            this.txtWorker.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtWorker.Location = new System.Drawing.Point(349, 9);
+            this.txtWorker.Name = "txtWorker";
+            this.txtWorker.Size = new System.Drawing.Size(268, 39);
+            this.txtWorker.TabIndex = 15;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Turquoise;
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(349, 341);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(210, 43);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Turquoise;
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(565, 341);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(195, 43);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtWorker);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.cboBox);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.txtFirstname);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.txtLastname);
+            this.panel1.Controls.Add(this.txtMail);
+            this.panel1.Controls.Add(this.txtAddress);
+            this.panel1.Location = new System.Drawing.Point(3, 54);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(858, 404);
+            this.panel1.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.ClientSize = new System.Drawing.Size(864, 718);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Registration form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtFirstname;
+        private System.Windows.Forms.TextBox txtLastname;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox cboBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtWorker;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace insertand_fetchdata
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getData();
+        }
+
+
+        SqlConnection conn = new SqlConnection("Data Source=ABIODUN\\JOHNSERVER;Initial Catalog=worker;Integrated Security=True;Encrypt=False");
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            SqlCommand cmd;
+            string workerid = txtWorker.Text;
+            string firstname = txtFirstname.Text;
+            string lastname = txtLastname.Text;
+            string status = cboBox.SelectedItem.ToString();
+            string address = txtAddress.Text;
+            string mail = txtMail.Text;
+            try
+            {
+
+                conn.Open();
+                cmd = new SqlCommand(@"INSERT INTO[dbo].[workers]([workerid],[firstname],[lastname],[status],[address],[mail])VALUES('" + workerid + "','" + firstname + "', '" + lastname + "', '" + status + "', '" + address + "', '" + mail + "') ", conn);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                MessageBox.Show("Data Inserted", "Error detected", MessageBoxButtons.YesNo);
+                getData();
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+        private void getData()
+        {
+            conn = new SqlConnection("Data Source=ABIODUN\\JOHNSERVER;Initial Catalog=worker;Integrated Security=True;Encrypt=False");
+            SqlCommand com = new SqlCommand(@"SELECT * FROM [dbo].[workers]", conn);
+            SqlDataAdapter adapter = new SqlDataAdapter(com);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+        }
+        private void getUpdate()
+        {
+            string workerid = txtWorker.Text;
+            string firstname = txtFirstname.Text;
+            string lastname = txtLastname.Text;
+            string status = cboBox.SelectedItem.ToString();
+            string address = txtAddress.Text;
+            string mail = txtMail.Text;
+            conn = new SqlConnection("Data Source=ABIODUN\\JOHNSERVER;Initial Catalog=worker;Integrated Security=True;Encrypt=False");
+            conn.Open();
+            SqlCommand com = new SqlCommand("UPDATE [dbo].[workers] SET[workerId] = '" + txtWorker.Text + "' , [firstname] = '" + txtFirstname.Text + "', [lastname] = '" + txtLastname.Text + "', [status] = '" + cboBox.SelectedItem.ToString() + "', [address] = '" + txtAddress.Text + "', [mail] = '" + txtMail.Text + "' WHERE [workerid]= '" + txtWorker.Text + "'  ", conn);
+            com.ExecuteNonQuery();
+            MessageBox.Show("Data Updated", "Error detected", MessageBoxButtons.YesNo);
+            conn.Close();
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            getUpdate();
+            getData();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            getDelete();
+            getData();
+
+        }
+        public void getDelete()
+        {
+            conn = new SqlConnection("Data Source=ABIODUN\\JOHNSERVER;Initial Catalog=worker;Integrated Security=True;Encrypt=False");
+            conn.Open();
+            SqlCommand com = new SqlCommand(@"DELETE FROM[dbo].[workers] WHERE [workerid]= '" + txtWorker.Text + "'" , conn);
+            com.ExecuteNonQuery();
+            MessageBox.Show("Data Deleted","",MessageBoxButtons.YesNo);
+            conn.Close();
+
+        }
+    }
+}
+
